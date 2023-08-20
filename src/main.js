59a2cd1f7c82910f0aaf054166ad58307afb5c5c
Olga Mamonova <shamrilo91@mail.ru>
2023-08-21 01:56:50 +0300

    правит замечание линта

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import { RenderPosition, render } from './render';
 import TripInfoView from './view/trip-info-view';
 import FilterView from './view/filter-view';
 import BoardPresenter from './presenter/board-presenter';
-import { getPoint } from './mocks/point';
-// import { getRandomInRange } from './utils';
-// import { generateOffer} from './mocks/point';
-// import { TYPES } from './mocks/const';
+// import { getPoint } from './mocks/point';
+// // import { getRandomInRange } from './utils';
+// // import { generateOffer} from './mocks/point';
+// // import { TYPES } from './mocks/const';
 
-console.log(getPoint());
 const tripInfoElement = document.querySelector('.trip-main');
 const filterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
