c5dfdb61a7f9c8090bf43fbb0c7c0d13fc9e66ab
Olga Mamonova <shamrilo91@mail.ru>
2023-09-10 13:21:35 +0300

    добавляет возможность переиспользования

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,7 +1,7 @@
 import EventEditView from '../view/event-edit-view';
 import PointView from '../view/point-view';
 
-import { render, replace } from '../framework/render';
+import { render, replace, remove } from '../framework/render';
 
 export default class PointPresenter {
   #container = null;
@@ -21,6 +21,9 @@ export default class PointPresenter {
   init(point) {
     this.#point = point;
 
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#editPointComponent;
+
     this.#pointComponent = new PointView({
       point: this.#point,
       pointDestination: this.#destinationsModel.getById(point.destination),
@@ -36,7 +39,26 @@ export default class PointPresenter {
       onHideBtnClick: this.#handleHideBtnClick
     });
 
-    render(this.#pointComponent, this.#container);
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#container);
+      return;
+    }
+
+    if (this.#container.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#container.contains(prevPointEditComponent.element)) {
+      replace(this.#pointComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  }
+
+  destroy(){
+    remove(this.#pointComponent);
+    remove(this.#editPointComponent);
   }
 
   #replacePointToForm() {
