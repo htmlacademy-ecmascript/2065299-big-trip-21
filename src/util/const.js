c97141ac791c7777c72e6b311b8ed93c6e22a27a
Olga Mamonova <shamrilo91@mail.ru>
2023-10-04 23:42:28 +0300

    правки замечания

--- src/util/const.js
+++ src/util/const.js
@@ -77,7 +77,7 @@ const NoPointText = {
 };
 
 
-const MODE = {
+const Mode = {
   DEFAULT: 'DEFAULT',
   EDITING: 'EDITING',
 };
@@ -89,39 +89,6 @@ const Method = {
   DELETE: 'DELETE',
 };
 
-const adaptToClient = (point) => {
-  const adaptedPoint = {
-    ...point,
-    dateFrom: point['date_from'],
-    dateTo: point['date_to'],
-    basePrice: point['base_price'],
-    isFavorite: point['is_favorite'],
-  };
-
-  delete adaptedPoint['date_from'];
-  delete adaptedPoint['date_to'];
-  delete adaptedPoint['base_price'];
-  delete adaptedPoint['is_favorite'];
-
-  return adaptedPoint;
-};
-
-const adaptToServer = (point) => {
-  const adaptedPoint = {
-    ...point,
-    ['date_from']: new Date(point.dateFrom).toISOString(),
-    ['date_to']: new Date(point.dateTo).toISOString(),
-    ['base_price']: point.basePrice,
-    ['is_favorite']: point.isFavorite,
-  };
-
-  delete adaptedPoint.dateFrom;
-  delete adaptedPoint.dateTo;
-  delete adaptedPoint.basePrice;
-  delete adaptedPoint.isFavorite;
-
-  return adaptedPoint;
-};
 
 const TimeLimit = {
   LOWER_LIMIT: 350,
@@ -141,9 +108,7 @@ export {
   NoPointText,
   EditType,
   POINT_EMPTY,
-  MODE,
+  Mode,
   Method,
-  adaptToClient,
-  adaptToServer,
   TimeLimit
 };
